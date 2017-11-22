518e774b24b214ef8fcd9882ef25dcad869a4a72
ZhangHongbo <157059449@qq.com>
2017-11-22 16:15:24 +0800

    delete partial comments.

--- src/main/java/cn/com/zhanghb/SpringBoot/Application.java
+++ src/main/java/cn/com/zhanghb/SpringBoot/Application.java
@@ -3,8 +3,6 @@ package cn.com.zhanghb.SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import cn.com.zhanghb.SpringBoot.DataBase.Service.DBSimulator;
-
 /**
  * Hello world!
  *
@@ -12,14 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application
 {
-	// private static DBSimulator dbSimulator = new DBSimulator();
-
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Application.class, args);
-		/*
-		 * try { dbSimulator.run("args0"); } catch (Exception e) {
-		 * System.out.println(e.getMessage().toString()); }
-		 */
 	}
 }
