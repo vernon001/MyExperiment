2016d36fa2b1109c657b715a69e050327edcaaf5
ZhangHongbo <157059449@qq.com>
2018-01-12 14:46:09 +0800

    modified the formater

--- src/main/java/cn/com/zhanghb/SpringBoot/Configuration/MvcConfig.java
+++ src/main/java/cn/com/zhanghb/SpringBoot/Configuration/MvcConfig.java
@@ -13,12 +13,10 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter
-{
+public class MvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
-	public void addViewControllers(ViewControllerRegistry registry)
-	{
+	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/hello").setViewName("hello");
